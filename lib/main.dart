7b5bdb9ff71a37fe28927d8f13458e4a17d2de23
arora <aroramadhav65@gmail.com>
2022-04-18 23:09:51 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:snapchat_ui/pages/root_app.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RootApp(),
-  ),);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootApp(),
+    ),
+  );
 }
